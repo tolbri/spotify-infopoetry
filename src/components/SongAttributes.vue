493442be9eb7061fa5d91cfe914d089d6c4cc0e8
tolbri <65823209+tolbri@users.noreply.github.com>
2020-12-31 00:13:01 +0100

    Resize Cover

--- src/components/SongAttributes.vue
+++ src/components/SongAttributes.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home-content">
-    <div class="flex flex-end mt-10">
-    <img src="./../assets/albumcover01.jpg" alt="" class="h-56 w-56 mr-10 shadow-xl" />
+    <div class="flex flex-end mt-10 p-5 ">
+      <transition name="expand">
+        <div v-bind:class="{ enlarge: expand }" class="cover bg-light mr-10 shadow-lg">
+      <!--VISUALIZATION-->
+        </div>
+      </transition>
     <div class="flex1 mt-auto">
       <strong class="uppercase tracking-widest text-white font-light text-xs">Library</strong>
       <h2 class="mb-5 text-5xl font-semibold text-white">Song Attributes</h2>
       <p>Created by <span class="text-white">Tim Olbrich</span> • 2000 Songs, 20 h 30 min.</p>
-      <button class="px-8 py-2 mt-5 bg-main rounded-full uppercase tracking-wide text-white  text-xs">Zoom</button>
+      <button @click="expand = !expand" class="px-8 py-2 mt-5 bg-main rounded-full uppercase tracking-wide text-white text-xs">{{expand ? 'Close' : 'Open'}}</button>
     </div>
     </div>
 
@@ -19,10 +23,28 @@
 
 <script>
 export default {
-name: "SongAttributes"
+name: "SongAttributes",
+  data() {
+  return {
+    expand: false
+  }
+  }
 }
 </script>
 
 <style scoped>
+.cover {
+  width: 14rem;
+  height: 14rem;
+  transition: all .5s;
+}
+
+.cover.enlarge {
+  width: 30rem;
+  height: 30rem;
+}
+
+
+
 
 </style>
